437895fe284397186753dc320c01c04bb59db789
vkheroim <77112266+vkheroim@users.noreply.github.com>
2024-04-04 21:17:36 +0000

    Deploying to gh-pages from @ vkheroim/goit-js-hw-12@fd9e51ca30228f3ac1c488c5f20da1aa6db8b7f2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,31 +1,24 @@
-import{S as f,i as a}from"./assets/vendor-3fe00192.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();function h(o){const r=document.querySelector(".gallery"),l=o.map(({webformatURL:e,largeImageURL:t,tags:i,likes:u,views:d,comments:p,downloads:m})=>`
-   <div class="galleryItem-wrapper">
-        <div class="galleryCard-wrapper">
-            <li class="gallery_items">
-                <a href="${t}">
-                <img src="${e}" alt="${i}" class="galleryItems-photo"></a>
-                <div class="gallery-textWrapper">
-                    <ul class="galleryItems-list">
-                        <li class="galleryItem">
-                            <h2>Likes</h2>
-                            <p>${u}</p>
-                        </li>
-                        <li class="galleryItem">
-                            <h2>Views</h2>
-                            <p>${d}</p>
-                        </li>
-                        <li class="galleryItem">
-                            <h2>Comments</h2>
-                            <p>${p}</p>
-                        </li>
-                        <li class="galleryItem">
-                            <h2>Downloads</h2>
-                            <p>${m}</p>
-                        </li>
-                    </ul>
-                </div>
-                
-            </li>
-        </div>
-    </div>`).join("");r.insertAdjacentHTML("afterbegin",l),new f(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom",widthRatio:.9,heightRatio:.8}).refresh()}function y(o){const r="43217946-108b18fb86fe71a4135c25a24",l="https://pixabay.com/api/",s=new URLSearchParams({key:r,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`${l}?${s}`).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()}).then(e=>{e.total||a.error({title:"Error",position:"topRight",message:"Вибачте, за вашим пошуковим запитом не знайдено зображень. Спробуйте ще раз!"}),h(e.hits)}).catch(e=>{a.error({title:"Error",position:"topRight",message:"Упс! Щось пішло не так!"})}).finally(()=>loader.hidden=!0)}const n=document.querySelector(".search-form"),g=document.querySelector(".gallery"),c=document.querySelector(".loader");n.addEventListener("submit",L);c.hidden=!0;function L(o){o.preventDefault(),g.innerHTML="",c.hidden=!1;const{searchRequest:r}=o.currentTarget.elements;let l=r.value;y(l),n.reset()}
+import{a as v,S as P,i as l}from"./assets/vendor-eded45c0.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function s(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function n(t){if(t.ep)return;t.ep=!0;const o=s(t);fetch(t.href,o)}})();async function g(r,e){const s="https://pixabay.com",n="/api/",t={key:"43217946-108b18fb86fe71a4135c25a24",q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:e},o=`${s}${n}`;return(await v.get(o,{params:t})).data}function m(r){return r.map(e=>`<li class="gallery-item">
+      <a class="gallery-link" href="${e.largeImageURL}">
+        <img class="gallery-image" src="${e.webformatURL}" alt="${e.tags}" />
+      </a>
+      <ul class="img-description">
+        <li>
+          <h3>Likes</h3>
+          <p>${e.likes}</p>
+        </li>
+        <li>
+          <h3>Views</h3>
+          <p>${e.views}</p>
+        </li>
+        <li>
+          <h3>Comments</h3>
+          <p>${e.comments}</p>
+        </li>
+        <li>
+          <h3>Downloads</h3>
+          <p>${e.downloads}</p>
+        </li>
+      </ul>
+    </li>`).join("")}const p=document.querySelector(".form"),u=document.querySelector(".gallery"),d=document.querySelector(".btn"),y=document.querySelector(".loader");let c,i=1,L=0;const S=15;let h=new P(".gallery a",{captionsData:"alt",captionDelay:250});h.on("show.simplelightbox",function(){});function b(){i>=L?(w(),l.warning({message:"Вибачте, але ви дійшли до кінця результатів пошуку.",color:"blue",position:"topRight"})):R()}function $(){const r=u.firstChild.getBoundingClientRect().height;console.log(r),scrollBy({top:r*2,behavior:"smooth"})}function R(){return d.classList.remove("is-hidden")}function w(){return d.classList.add("is-hidden")}function M(){return y.classList.remove("is-hidden")}function f(){return y.classList.add("is-hidden")}f();w();p.addEventListener("submit",async r=>{if(r.preventDefault(),c=r.target.elements.search.value.trim(),c==="")return l.warning({message:"Будь ласка, заповніть поле!",color:"red",position:"topRight"});u.innerHTML="",i=1,M();try{const e=await g(c,i);L=Math.ceil(e.totalHits/S);const s=m(e.hits);e.hits.length===0?l.error({message:"На жаль, за вашим запитом не знайдено зображень. Будь ласка, спробуйте ще раз!",position:"topRight",color:"red"}):(u.insertAdjacentHTML("beforeend",s),h.refresh(),b())}catch{l.error({message:"На жаль, за вашим запитом не знайдено зображень. Будь ласка, спробуйте ще раз!",position:"topRight",color:"red"})}finally{f(),p.reset()}});d.addEventListener("click",async r=>{M(),i+=1;const e=await g(c,i),s=m(e.hits);u.insertAdjacentHTML("beforeend",s),$(),h.refresh(),f(),b()});
 //# sourceMappingURL=commonHelpers.js.map
